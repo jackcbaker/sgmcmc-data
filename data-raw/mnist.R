e2f53532e009df3ca74d39e6a9560cefe246ba8b
Jack Baker <j.baker1@lancaster.ac.uk>
2017-09-25 14:26:50 +0100

    First commit

---
+++ data-raw/mnist.R
@@ -0,0 +1,11 @@
+library(tensorflow)
+# Load MNIST dataset from TensorFlow datasets
+tfData = tf$contrib$learn$datasets
+mnistTF = tfData$mnist$read_data_sets("MNIST-data", one_hot = TRUE)
+# Rebuild dataset as list
+mnist = list() 
+mnist$train = list( "images" = mnistTF$train$images, "labels" = mnistTF$train$labels )
+mnist$test = list( "images" = mnistTF$test$images, "labels" = mnistTF$test$labels )
+# Delete downloaded files
+message("Deleting temporary files")
+unlink("MNIST-data", recursive = TRUE)
